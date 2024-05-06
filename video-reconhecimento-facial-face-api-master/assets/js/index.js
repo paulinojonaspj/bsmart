904f28e5daaf24946248b4e5d972dcf17769708b
Paulino Jonas <37410557+paulinojonaspj@users.noreply.github.com>
2024-05-07 00:06:24 +0100

    DER

---
+++ video-reconhecimento-facial-face-api-master/assets/js/index.js
@@ -0,0 +1,82 @@
+const cam = document.getElementById('cam')
+
+const startVideo = () => {
+    navigator.mediaDevices.enumerateDevices()
+        .then(devices => {
+            if (Array.isArray(devices)) {
+                navigator.getUserMedia(
+                    {
+                        video: {
+                            deviceId: devices[0].deviceId
+                        }
+                    },
+                    stream => cam.srcObject = stream,
+                    error => console.error(error)
+                )
+            }
+        })
+}
+
+const loadLabels = () => {
+    const labels = ['Paulino Jonas', 'Jacira Chicola', 'Paulina Jonas']
+    return Promise.all(labels.map(async label => {
+        const descriptions = []
+        for (let i = 1; i <= 5; i++) {
+            const img = await faceapi.fetchImage(`/assets/lib/face-api/labels/${label}/${i}.png`)
+            const detections = await faceapi
+                .detectSingleFace(img)
+                .withFaceLandmarks()
+                .withFaceDescriptor()
+                if (detections) {
+            descriptions.push(detections.descriptor)
+                }
+        }
+        return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    }))
+}
+
+Promise.all([
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/lib/face-api/models'),
+]).then(startVideo)
+
+cam.addEventListener('play', async () => {
+    const canvas = faceapi.createCanvasFromMedia(cam)
+    const canvasSize = {
+        width: cam.width,
+        height: cam.height
+    }
+    const labels = await loadLabels()
+    faceapi.matchDimensions(canvas, canvasSize)
+    document.body.appendChild(canvas)
+    setInterval(async () => {
+        const detections = await faceapi
+            .detectAllFaces(
+                cam,
+                new faceapi.SsdMobilenetv1Options()
+            )
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withFaceDescriptors()
+        const resizedDetections = faceapi.resizeResults(detections, canvasSize)
+        const faceMatcher = new faceapi.FaceMatcher(labels, 0.6)
+        const results = resizedDetections.map(d =>
+            faceMatcher.findBestMatch(d.descriptor)
+        )
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+
+        results.forEach((result, index) => {
+            const box = resizedDetections[index].detection.box
+            const { label, distance } = result
+            new faceapi.draw.DrawTextField([
+                `${label} (${parseInt(distance * 100, 10)})`
+            ], box.bottomRight).draw(canvas)
+        })
+    }, 100)
+})
